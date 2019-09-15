39f1303853fe7c3d56332dd6dba34cec22a0b9eb
rsrawley <rsrawley@gmail.com>
2019-09-15 03:32:23 +0100

    Initial commit

---
+++ push.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Function definition
+push_updates () {
+        git add . # add all files to be tracked
+        git commit -a # commit all changes
+        git push origin master # master branch
+}
+
+
+cd libraries
+push_updates # submodule update
+
+cd ..
+push_updates # main repo update
