cd2e00cce69f4a84d76aff759fa465d2e1f89b47
rsrawley <rsrawley@gmail.com>
2019-09-17 01:32:50 +0100

    Added 3v3 and high pin for RE/DE

--- spa.js
+++ spa.js
@@ -1,9 +1,21 @@
+// Set up GPIO access
+let gpio = require('onoff').Gpio;
+let re_de = new gpio(4,'high'); // Physical pin 7
+let Vcc = new gpio(17,'high'); // Physical pin 11
+
+// Release all GPIOs on exit
+process.on('SIGINT', function () {
+	re_de.unexport();
+	Vcc.unexport();
+	process.exit()
+})
+
+
+// Set up serial port
 const SerialPort = require('serialport')
 const port = new SerialPort('/dev/ttyAMA0', {
   baudRate: 19200
 })
-
-//sendData("test");
 
 function sendData(data) {
 	console.log("Sending: " + data);
@@ -17,10 +29,13 @@ function sendData(data) {
 
 // Open errors will be emitted as an error event
 port.on('error', function(err) {
-  console.log('Error: ', err.message)
+  console.log('Open Error: ', err.message) // ??? Will this ever happen since I left AutoOpen to default???
 })
 
 // Switches the port into "flowing mode"
 port.on('data', function (data) {
   console.log('Data received:', data)
 })
+
+//sendData("test");
+console.log("Ready!");
