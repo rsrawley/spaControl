76826ddef0a83613f2df259d339346f1fa8b471b
rsrawley <rsrawley@gmail.com>
2019-10-11 00:57:53 +0100

    Adding message types

--- spa.js
+++ spa.js
@@ -1,21 +1,20 @@
 // Set up GPIO access
 let gpio = require('onoff').Gpio;
-//let Vcc = new gpio(18,'high'); // Physical pin 12
-let RE = new gpio(23,'low'); // Physical pin 16
-let DE = new gpio(24,'low'); // Physical pin 18
+let Vcc = new gpio(18,'high'); // Physical pin 12
+let RE_DE = new gpio(23,'low'); // Physical pin 16
+
 
 // Release all GPIOs on exit
 process.on('SIGINT', function () {
 	Vcc.unexport();
-	RE.unexport();
-	DE.unexport();
+	RE_DE.unexport();
 	process.exit()
 })
 
 
 // Set up message translation matrix
-let translate = {
-	"ffaf13" : {
+let translate = { // Status update
+	"ff af 13" : { 
 		"description" : "Status udpate",
 		"codeLine" : "00 F1 CT HH MM F2 00 00 00 F3 F4 PP 00 CP LF 00 00 00 00 00 ST 00 00 00".split(" "),
 		"codes" : {
@@ -33,6 +32,51 @@ let translate = {
 		}
 	},
 	
+	"10 bf 06" : {
+		"description" : "Ready for command???" // Not confirmed
+	},
+
+	"10 bf 07" : {
+		"description" : "No command to send???" // Not confirmed
+	},
+
+	"0a bf 94" : { // Control configuration?? Not confirmed
+		
+	},
+
+	"0a bf 2e" : { // Control configuration 2?? Not confirmed
+		
+	},
+	"" : {
+		
+	},
+	"" : {
+		
+	},
+	"" : {
+		
+	},
+	"" : {
+		
+	},
+	"" : {
+		
+	},
+	"" : {
+		
+	},
+	"" : {
+		
+	},
+	"" : {
+		
+	},
+	"" : {
+		
+	},
+	"" : {
+		
+	},	
 /*	"10bf06" : {
 		"description" : "Unknown1",
 	},
@@ -43,43 +87,41 @@ let translate = {
 }
 
 // Set up serial port
-
-let temp=0;
-
 const SerialPort = require('serialport');
 const Delimiter = require('@serialport/parser-delimiter');
 const port = new SerialPort('/dev/ttyAMA0', {
   baudRate: 115200
 });
 const parser = port.pipe(new Delimiter({ delimiter: Buffer.from('7e', 'hex') }));
+
 parser.on('data', function(data) {
 	data = data.hexSlice(); // Convert to hexadecimal string
-
-	//data = "20ffaf131403610c2d000a6161040c000002000000000124600100001e00004a"
 
 	// Extract message length (first byte) and message type (next 3 bytes)
 	let message = {
 		"hex" : data.match(/../g).join(" "),
-		"length" : parseInt(data.substr(0,2),16), // Number of bytes (2 characters in hex per byte, so number of characters is twice this number)
-		"type" : data.substr(2,6),
-		"content" : data.substr(8).match(/../g) // Slice to the end and put into array, split two characters at a time (the hex code)
+		"length" : parseInt(data.substr(0,2),16), // First byte is length (number of bytes in message) (2 characters in hex per byte, so number of characters is twice this number)
+		"type" : data.substr(2,6), // Next 6 bytes is type of message
+		"content" : data.substring(8,data.length-2).match(/../g) // Slice to the end (except the checksum) and put into array, split two characters at a time (the hex code)
+		"checksum" : data.substr(-2,2) // Last byte is checksum
 	}
 
-	if (data.length != message.length*2) {
-		console.log("Message length error!", data.length, message.length*2)
-	} else {
+	if (data.length == message.length*2 && checksum(data.substring(0,data.length-2)) == message.checksum) { // Check proper message length and checksum
+		
+		// Insert spaces every two characters to match "human readable" object type defined at top of program
+		message.type = message.type.substr(0,2) + " " + message.type.substr(2,2) + " " + message.type.substr(4,2);
+		
 		// Translate message
 		if (message.type in translate) {
-			console.log(translate[message.type].description)
+//console.log(translate[message.type].description)
 
 			let codeLine = translate[message.type].codeLine; // Order of codes
 			let codes = translate[message.type].codes; // Translation of codes
+
 			// Go through message content and translate byte by byte
 			for (let i=0; i<message.length; i++) {
-				if (codeLine[i] in codes) { // If a code exists in codeLine, try to display it
-					if (codeLine[i] == "ST") {
+				if (codeLine[i] in codes) { // If a code exists in codeLine, store in memory
 					console.log(codes[codeLine[i]] + " ==> " + parseInt(message.content[i],16))
-					}
 				}
 			}
 		}
@@ -97,7 +139,6 @@ if (message.type == "10bf06" && temp==0) {
 	//	sendData('0610bf2061c9') //06 10 bf 20 61 c9
   sendData('10bf2060') //06 10 bf 20 61 c9
 
-		temp++
 
 }
 
@@ -118,44 +159,38 @@ function sendData(data) {
 	let length = data.length/2 + 2;
 	data = length.toString(16).padStart(2,"0") + data;
 
-	// Computer CRC checksum
+	// Compute CRC8 checksum
 	let crc = checksum(data);
-	data = data + crc.toString(16).padStart(2,"0");
+	data = data + crc;
 
 	// Append message start and end bytes
 	data = "7e" + data + "7e"; 
-console.log("hexstring: " +data) // for testing only
+
 	// Change HEX string to ASCII characters	
 	let asciiString="";
 	for (let i=0; i<data.length/2; i++) {
 		asciiString = asciiString + String.fromCharCode(parseInt(data.substr(i*2,2),16))
 	}
-	//console.log(message) // For testing only
 
 	transmit(asciiString)() // Returns a function that needs to be executed right away
 }
 
 
-
 function transmit(asciiString) {
 	return 	function() {
-		DE.write(1, function() { // Switch RS485 module to transmit
-			RE.write(1, function() {
+		RE_DE.write(1, function() { // Switch RS485 module to transmit
+			port.write(asciiString, 'ascii', function(err) {
+			  if (err) {
+			    return console.log('Error on write: ', err.message)
+			  }
 
-				port.write(asciiString, 'ascii', function(err) {
-					console.log("port.write sending:"+asciiString)
-				  if (err) {
-				    return console.log('Error on write: ', err.message)
-				  }
-
-				  // Switch RS485 module back to receive
-				  DE.write(0)
-				  RE.write(0)
-				})
+			  // Switch RS485 module back to receive
+			  RE_DE.write(0);
 			})
-		})
+		})		
 	}
 }
+
 
 function checksum(hexstring) {
 	// web site for CRC checksum : http://www.sunshine2k.de/coding/javascript/crc/crc_js.html
@@ -182,47 +217,10 @@ function checksum(hexstring) {
 	let crc = 2;
 
   for (let i=0; i<hexstring.length/2; i++) {
-    const byte = parseInt(hexstring.substr(i*2,2),16)
+    const byte = parseInt(hexstring.substr(i*2,2),16);
     crc = TABLE[(crc ^ byte) & 0xff] & 0xff;
   }
-  crc^=2
+  crc^=2;
 
-  return crc.toString(16);
+  return crc.toString(16).padStart(2,"0");
 }
-
-
-/*
-const SerialPort = require('serialport')
-const port = new SerialPort('/dev/ttyAMA0', {
-  baudRate: 115200
-})
-
-
-
-// Open errors will be emitted as an error event
-port.on('error', function(err) {
-  console.log('General error: ', err.message) // ??? Will this ever happen since I left AutoOpen to default???
-})
-
-// Switches the port into "flowing mode"
-port.on('data', function (data) {
-  console.log(data); // data.toString() to get ASCII character OR data.hexSlice() to get straight hexadecimal
-})
-
-
-console.log("Ready!");
-//sendData("test");
-
-// HEX to decimal and ASCII
-//https://www.rapidtables.com/convert/number/ascii-to-hex.html
-
-
-	let translation="";
-
-	let message = data.hexSlice();
-
-	for (key in data) {
-		translation+=key+"\n"
-	}
-	console.log(translation);
-*/
